dff06b5e13311b6ddbeb6d8f71a64649d11841fe
ElDelOtroDia <luis_carrillo_r@hotmail.com>
2020-03-06 07:58:43 -0600

    Correccion de routling a fretboard

--- src/app/select-escala/select-escala.page.ts
+++ src/app/select-escala/select-escala.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-select-escala',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectEscalaPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  Fretboard() {
+    this.navCtrl.navigateForward("/fretboard");
   }
 
 }
